ad8bce6da7de646312a9633fe48cf46bf36f8487
giorgiospirostopala <gio.topala@gmail.com>
2025-03-08 19:55:32 +0100

    add: detail pages with dynamic titles

--- src/pages/SinglePost.jsx
+++ src/pages/SinglePost.jsx
@@ -1,7 +1,32 @@
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const SinglePost = () => {
+
+    const { id } = useParams();
+    const [post, setPost] = useState(
+        {
+            id: "",
+            title: "",
+            content: "",
+            img: "",
+            tags: []
+        }
+    );
+    const url = import.meta.env.VITE_ENDPOINT_URL;
+
+    useEffect(() => {
+        axios
+            .get(url + "/" + id)
+            .then((res) => setPost(res.data))
+            .catch((err) => console.error(err));
+    }, [id])
+
     return (
-        <h1>Singolo post</h1>
+        <h1>{post.title}</h1>
     )
+
 };
 
 export default SinglePost;
